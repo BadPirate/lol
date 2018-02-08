54d0eac6d115a3698c846d539a78e0c3d770f691
Kevin Lohman <badpirate@gmail.com>
2018-02-07 18:00:29 -0800

    Add existing Loki Ledger HTML5 Up and update sub licenses.  Resolves #2

--- README.md
+++ README.md
@@ -36,6 +36,6 @@ In order to send money you must create a transaction (which requires internet ac
 8. Scan in your signed transaction to Loki and broadcast to bitcoin network
 
 ### But wait!
-Savvy security folks might have noticed that since it's kind of hard for a human to read a QR code, Frosty could be sending your private key hidden in the signed transaction code and Loki could pick it up.  Good on you.  That vulnerability only works if both Loki and Frosty are colluding :)  Which is why I encourage you to check out the source and launch your own version of Loki (either locally on your own laptop or using the heroku button below).s
+Savvy security folks might have noticed that since it's kind of hard for a human to read a QR code, Frosty could be sending your private key hidden in the signed transaction code and Loki could pick it up.  Good on you.  That vulnerability only works if both Loki and Frosty are colluding :)  Which is why I encourage you to check out the source and launch your own version of Loki (either locally on your own laptop or using the [Heroku](https://heroku.com) button below).
 
 [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
---
+++ send.php
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML>
+<!--
+	Miniport by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>LOL - Send Transaction</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="container">
+					<li><a href="index.php#top">Loki</a></li>
+					<li><a href="index.php#about">About Trustless Transactions</a></li>
+					<li><a href="login">Send BTC</a></li>
+				</ul>
+			</nav>
+
+		<!-- Home -->
+			<div class="wrapper style1 first">
+				<article class="container" id="top">
+					<div class="row">
+						<div class="8u 12u(mobile)">
+							<header>
+								<h1>Step 1: Scan from address</h1>
+                <p>Scan the <i>public</i> address of the wallet you'd like to send
+                  money from.</p>
+							</header>
+						</div>
+					</div>
+				</article>
+			</div>
+
+		<!-- Contact -->
+			<div class="wrapper style4">
+					<footer>
+						<ul id="copyright">
+							<li>&copy; Logic High Software. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						</ul>
+					</footer>
+				</article>
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
